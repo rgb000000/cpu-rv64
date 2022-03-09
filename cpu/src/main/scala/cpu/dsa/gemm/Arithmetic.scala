47077ab718c8c46ba40354c0b6ef87485f4f9a0f
rgb000000 <rgb000000black@gmail.com>
2022-03-09 23:39:53 +0800

    feat: add >> and clipped ops

--- cpu/src/main/scala/cpu/dsa/gemm/Arithmetic.scala
+++ cpu/src/main/scala/cpu/dsa/gemm/Arithmetic.scala
@@ -9,6 +9,8 @@ abstract class ArithmeticOps[T <: Data](self: T){
   def +(t: T): T
   def *(t: T): T
   def mac(m1: T, m2: T): T
+  def clipped2witdhOf(t: T): T
+  def >>(u: UInt): T
 }
 
 abstract class Arithmetic[T <: Data] {
@@ -22,6 +24,11 @@ object Arithmetic{
       override def +(t: UInt): UInt = self + t
       override def *(t: UInt): UInt = self * t
       override def mac(m1: UInt, m2: UInt): UInt = m1 * m2 + self
+      override def >>(u: UInt): UInt = (self >> u).asUInt()
+      override def clipped2witdhOf(t: UInt): UInt = {
+        val sat = ((1 << (t.getWidth)) - 1).U
+        Mux(self > sat, sat, self)(t.getWidth-1, 0).asUInt()
+      }
     }
   }
 
@@ -30,6 +37,13 @@ object Arithmetic{
       override def +(t: SInt): SInt = self + t
       override def *(t: SInt): SInt = self * t
       override def mac(m1: SInt, m2: SInt): SInt = m1 * m2 + self
+      override def >>(u: UInt): SInt = (self >> u).asSInt()
+      override def clipped2witdhOf(t: SInt): SInt = {
+        val max = ((1 << (t.getWidth - 1)) - 1).S
+        val min = (-(1 << (t.getWidth - 1))).S
+        Mux(self > max, max,
+        Mux(self < min, min, self))(t.getWidth-1, 0).asSInt()
+      }
     }
   }
 
