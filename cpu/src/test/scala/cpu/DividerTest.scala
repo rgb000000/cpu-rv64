e3412cccf4ab1b73ada33b4a4b3856d15df3ff2c
rgb000000 <rgb000000black@gmail.com>
2022-03-11 22:28:38 +0800

    feat: cp multiplier and divider from xiangshan

---
+++ cpu/src/test/scala/cpu/DividerTest.scala
@@ -0,0 +1,50 @@
+package cpu
+
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import chiseltest.internal.{VerilatorBackendAnnotation, WriteVcdAnnotation}
+import cpu.ooo.{Divider, Multiplier}
+import firrtl.options.TargetDirAnnotation
+import org.scalatest.FreeSpec
+
+class DividerTest extends FreeSpec with ChiselScalatestTester {
+  implicit val p = new DRAM3SimConfig
+  "Simple test for divider" in {
+    val annos = Seq(
+      TargetDirAnnotation("test_run_dir"),
+      //      VerilatorBackendAnnotation,
+      WriteVcdAnnotation,
+    )
+
+    RawTester.test(new Divider, annos){c =>
+      c.io.in.valid.poke(true.B)
+      c.io.in.bits.src(0).poke(0x10.U)
+      c.io.in.bits.src(1).poke(0x3.U)
+      c.io.in.bits.ctrl.sign.poke(false.B)
+      c.io.in.bits.ctrl.isW.poke(false.B)
+      c.io.in.bits.ctrl.isHi.poke(false.B)
+      c.io.kill.poke(false.B)
+
+      c.clock.step(10)
+
+      c.io.in.bits.ctrl.isHi.poke(true.B)
+      c.io.out.ready.poke(true.B)
+
+      c.clock.step(10)
+    }
+  }
+}
+
+object DividerLaunch extends App{
+  implicit val p = new DRAM3SimConfig
+  (new ChiselStage).execute(
+    Array(
+      "-X", "verilog",
+      "--target-dir", "test_run_dir"
+      //      "--help"
+    ),
+    Seq(ChiselGeneratorAnnotation(() => new Divider))
+  )
+}
---
+++ cpu/src/test/scala/cpu/MultiplierTest.scala
@@ -0,0 +1,53 @@
+package cpu
+
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import chiseltest.internal.{VerilatorBackendAnnotation, WriteVcdAnnotation}
+import cpu.ooo.Multiplier
+import firrtl.options.TargetDirAnnotation
+import org.scalatest.FreeSpec
+
+class MultiplierTest extends FreeSpec with ChiselScalatestTester {
+  implicit val p = new DRAM3SimConfig
+  "Simple test for multiplier" in {
+    val annos = Seq(
+      TargetDirAnnotation("test_run_dir"),
+      //      VerilatorBackendAnnotation,
+      WriteVcdAnnotation,
+    )
+
+    RawTester.test(new Multiplier, annos){c =>
+      c.io.in.valid.poke(true.B)
+      c.io.in.bits.src(0).poke(0xff.U)
+      c.io.in.bits.src(1).poke(0x100.U)
+      c.clock.step(10)
+
+      c.io.in.bits.src(1).poke(0x1000.U)
+      c.io.out.ready.poke(true.B)
+      c.clock.step(1)
+
+      c.io.out.ready.poke(false.B)
+      c.clock.step(10)
+
+      c.io.out.ready.poke(true.B)
+      c.clock.step(3)
+
+      c.io.out.ready.poke(false.B)
+      c.clock.step(10)
+    }
+  }
+}
+
+object MultiplierLaunch extends App{
+  implicit val p = new DRAM3SimConfig
+  (new ChiselStage).execute(
+    Array(
+      "-X", "verilog",
+      "--target-dir", "test_run_dir"
+      //      "--help"
+    ),
+    Seq(ChiselGeneratorAnnotation(() => new Multiplier))
+  )
+}
